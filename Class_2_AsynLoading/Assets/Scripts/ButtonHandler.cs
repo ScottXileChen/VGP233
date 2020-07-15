76bf44b908dfa82263d8fb99c374da74bbe378b0
ScottXileChen <53885585+ScottXileChen@users.noreply.github.com>
2020-07-15 11:45:56 +0800

    [VGP233]
    - Add scene change and call back function

---
+++ Class_2_AsynLoading/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void NavigateToGameScene()
+    {
+        Debug.Log("Navigating to the game scene...");
+        Loader.Load(Loader.Scene.GameScene);
+    }
+}
---
+++ Class_2_AsynLoading/Assets/Scripts/Loader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    public enum Scene
+    {
+        GameScene,
+        LoadingScene,
+    }
+
+    private static Action onLoaderCallback;
+    public static void Load(Scene scene)
+    {
+        onLoaderCallback = () =>
+        {
+            SceneManager.LoadScene(scene.ToString());
+        };
+
+       SceneManager.LoadScene(Loader.Scene.LoadingScene.ToString());
+    }
+
+    //Load the target scene
+    public static void LoaderCallBack()
+    {
+        if(onLoaderCallback != null)
+        {
+            onLoaderCallback();
+            onLoaderCallback = null;
+        }
+    }
+}
