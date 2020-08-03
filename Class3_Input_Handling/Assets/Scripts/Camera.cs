2268c873339374820aecab683f536f0e3e5da054
ScottXileChen <53885585+ScottXileChen@users.noreply.github.com>
2020-08-03 13:27:44 +0800

    [VGP233]
    Add mid term

---
+++ Class3_Input_Handling/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject target;
+    private float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position.y;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + offset, target.transform.position.z);
+    }
+}
---
+++ Midterm_Game/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    [SerializeField]
+    private float MaxSpeed = 0;
+    [SerializeField]
+    private float Accelerate = 0;
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(vertical, 0.0f, horizontal);
+        
+        rigidbody.AddForce(force * Time.deltaTime * Accelerate);
+    }
+}
