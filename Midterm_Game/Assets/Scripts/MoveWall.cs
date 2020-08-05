c4ce89b1b486144a42ece63e40ed057aa1ea5d66
ScottXileChen <53885585+ScottXileChen@users.noreply.github.com>
2020-08-05 09:53:07 +0800

    [VGP233]
    Update

---
+++ Midterm_Game/Assets/Scripts/MoveWall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWall : MonoBehaviour
+{
+    public float moveSpeed = 4f;
+    public int damage = 10;
+    private float hitTime = 2f;
+    private float hitTimer = 0f;
+    private void Update()
+    {
+        transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            hitTimer -= Time.deltaTime;
+            if (hitTimer <= 0)
+            {
+                hitTimer = hitTime;
+                PlayerManager.instance.player.GetComponent<PlayerState>().TakeDamage(damage);
+            }
+        }
+    }
+}
